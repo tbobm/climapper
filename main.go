a9156f689d36a3296c5ebff4f84739b21bb810cf
Alexis Coquard <alexiscoquard@MacBook-Pro-de-Alexis.local>
2017-02-27 15:07:05 +0100

    check args and construct url

--- main.go
+++ main.go
@@ -2,18 +2,37 @@ package main
 
 import (
 	"fmt"
-	"github.com/anaskhan96/soup"
 	"os"
+	"strings"
 )
 
+func makeUrl() string {
+	coords := checkParams()
+	if coords[0] != "-1" && coords[1] != "-1" {
+		return "https://citymapper.com/directions?endcoord=" + coords[0] + "%2C" + coords[1]
+	}
+	return "-1"
+}
+
+func checkParams() []string {
+	if len(os.Args) > 1 {
+		if len(os.Getenv(os.Args[1])) > 0 {
+			coords := strings.Split(os.Getenv(os.Args[1]), ",")
+			if len(coords) == 2 {
+				return coords
+			} else {
+				fmt.Println("Invalid format for environment variable", os.Args[1])
+			}
+		} else {
+			fmt.Println("No value found for environment variable", os.Args[1])
+		}
+	} else {
+		fmt.Println("Invalid usage")
+	}
+	return []string{"-1", "-1"}
+}
+
+
 func main() {
-	resp, err := soup.Get("https://xkcd.com")
-	if err != nil {
-		os.Exit(1)
-	}
-	doc := soup.HTMLParse(resp)
-	links := doc.Find("div", "id", "comicLinks").FindAll("a")
-	for _, link := range links {
-		fmt.Println(link.Text(), "| Link :", link.Attrs()["href"])
-	}
+	fmt.Println(makeUrl())
 }
