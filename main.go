038433fd8e338343781f26012de2c82c8b621eeb
Theo Massard <massar_t@etna-alternance.net>
2017-02-28 18:43:47 +0100

    [UPD] added environment variables and updated glide lock

--- main.go
+++ main.go
@@ -3,12 +3,11 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	//<<<<<<< HEAD
 	"io/ioutil"
 	"net/http"
 	"os"
+	"strings"
 	"time"
-	//	"reflect"
 )
 
 type Leg struct {
@@ -29,62 +28,61 @@ type Journey struct {
 	ListLegs []Legs
 }
 
+func checkParams() []string {
+	if len(os.Args) > 1 {
+		if len(os.Getenv(os.Args[1])) > 0 {
+			coords := strings.Split(os.Getenv(os.Args[1]), ",")
+			if len(coords) == 2 {
+				return coords
+			} else {
+				fmt.Println("Invalid format for environment variable", os.Args[1])
+				os.Exit(10)
+			}
+		} else {
+			fmt.Println("No value found for environment variable", os.Args[1])
+			os.Exit(10)
+		}
+	} else {
+		fmt.Println("Invalid usage")
+		os.Exit(3)
+	}
+	return "-1", "-1"
+}
+
+func makeUrl() string {
+	coords := checkParams()
+	return "https://citymapper.com/directions?endcoord=" + coords[0] + "%2C" + coords[1]
+}
+
 func main() {
 	var req string
+	var dat map[string]interface{}
 
 	req = "https://citymapper.com/api/7/journeys?start=48.813896%2C2.392448&end=48.928378%2C2.1622&sname=7%20Rue%20Maurice%20Grandcoing%2C%20Ivry-sur-Seine&ename=81%20Avenue%20de%20Tobrouk%2C%20Sartrouville&region_id=fr-paris"
-	//	fmt.Println(req)
 	resp, err := http.Get(req)
+
 	defer resp.Body.Close()
 	if err != nil {
 		os.Exit(1)
 	}
+
 	fmt.Println(resp.Body)
-	var dat map[string]interface{}
-	content, err := ioutil.ReadAll(resp.Body)
+
+	content, err := ioutil.ReadAll(resp.Body) // Content to bytes
+
 	if err != nil {
 		os.Exit(2)
 	}
+
 	if err := json.Unmarshal(content, &dat); err != nil {
 		panic(err)
 	}
 
-	//sample := dat["journeys"][0]["legs"]
+	sample := dat["journeys"][0]["legs"]
 	//fmt.Println("%v")
 	//	resp, err := soup.Get("https://xkcd.com")
 	//	if err != nil {
 	//		os.Exit(1)
 	//	}
 	//)
-
-	//func makeUrl() string {
-	//	coords := checkParams()
-	//	if coords[0] != "-1" && coords[1] != "-1" {
-	//		return "https://citymapper.com/directions?endcoord=" + coords[0] + "%2C" + coords[1]
-	//	}
-	//	return "-1"
-	//}
-	//
-	//func checkParams() []string {
-	//	if len(os.Args) > 1 {
-	//		if len(os.Getenv(os.Args[1])) > 0 {
-	//			coords := strings.Split(os.Getenv(os.Args[1]), ",")
-	//			if len(coords) == 2 {
-	//				return coords
-	//			} else {
-	//				fmt.Println("Invalid format for environment variable", os.Args[1])
-	//			}
-	//		} else {
-	//			fmt.Println("No value found for environment variable", os.Args[1])
-	//		}
-	//	} else {
-	//		fmt.Println("Invalid usage")
-	//	}
-	//	return []string{"-1", "-1"}
-	//}
-	//
-	//
-	//func main() {
-	//	fmt.Println(makeUrl())
-	//>>>>>>> 65a1a61c5c340d929a9d290c4c671b394abea9cb
 }
