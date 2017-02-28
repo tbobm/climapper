d4cc206117c926bc3abd3ffad7ff789a2ed6b4ca
Theo Massard <massar_t@etna-alternance.net>
2017-02-28 14:39:44 +0100

    [UPD] yes

--- main.go
+++ main.go
@@ -1,19 +1,57 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
-	"github.com/anaskhan96/soup"
+	"io/ioutil"
+	"net/http"
 	"os"
+	"time"
+	//	"reflect"
 )
 
+type Leg struct {
+	Mode              string    `json:"mode"`
+	ArrivalTime       time.Time `json:"arrival_time"`
+	DepartureTime     time.Time `json:"departure_time"`
+	DistanceMeters    int       `json:"distance_meters"`
+	Duration          int       `json:"duration_seconds"`
+	InStation         int       `json:"in_station_seconds"`
+	InStationWalkKind int       `json:"in_station_walk_kind"`
+}
+
+type Legs struct {
+	Legs []Leg
+}
+
+type Journey struct {
+	ListLegs []Legs
+}
+
 func main() {
-	resp, err := soup.Get("https://xkcd.com")
+	var req string
+
+	req = "https://citymapper.com/api/7/journeys?start=48.813896%2C2.392448&end=48.928378%2C2.1622&sname=7%20Rue%20Maurice%20Grandcoing%2C%20Ivry-sur-Seine&ename=81%20Avenue%20de%20Tobrouk%2C%20Sartrouville&region_id=fr-paris"
+	//	fmt.Println(req)
+	resp, err := http.Get(req)
+	defer resp.Body.Close()
 	if err != nil {
 		os.Exit(1)
 	}
-	doc := soup.HTMLParse(resp)
-	links := doc.Find("div", "id", "comicLinks").FindAll("a")
-	for _, link := range links {
-		fmt.Println(link.Text(), "| Link :", link.Attrs()["href"])
+	fmt.Println(resp.Body)
+	var dat map[string]interface{}
+	content, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		os.Exit(2)
 	}
+	if err := json.Unmarshal(content, &dat); err != nil {
+		panic(err)
+	}
+
+	//sample := dat["journeys"][0]["legs"]
+	//fmt.Println("%v")
+	//	resp, err := soup.Get("https://xkcd.com")
+	//	if err != nil {
+	//		os.Exit(1)
+	//	}
 }
