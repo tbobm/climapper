5aefcf5cc3a363c94be4a3d99ca846e592974817
Theo Massard <massar_t@etna-alternance.net>
2017-03-16 14:58:39 +0100

    [UPD] Added a test, and travis file

--- tests.py
+++ tests.py
@@ -15,3 +15,9 @@ def test_f_minutes_big():
 def test_f_minutes_with_round():
     res = climapper.f_minutes(289)
     assert res == 5
+
+
+def test_make_url():
+    url = climapper.make_url("48.813896%2C2.392448", "48.928378%2C2.162200")
+    valid_url = "https://citymapper.com/api/7/journeys?start=48.813896%2C2.392448&end=48.928378%2C2.162200&region_id=fr-paris"
+    assert url == valid_url
