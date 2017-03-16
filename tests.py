280136037e1f7000816e119159a10d205cbe5f6f
Theo Massard <massar_t@etna-alternance.net>
2017-03-16 17:42:05 +0100

    [UPD] added test get params

--- tests.py
+++ tests.py
@@ -1,6 +1,9 @@
 import climapper
 
 
+ENDPOINT = "api/7/journeys?start=FROM&end=TO&region_id=fr-paris"
+
+
 def test_f_minutes_big():
     """TODO: Docstring for test_.
 
@@ -19,5 +22,15 @@ def test_f_minutes_with_round():
 
 def test_make_url():
     url = climapper.make_url("48.813896%2C2.392448", "48.928378%2C2.162200")
-    valid_url = "https://citymapper.com/api/7/journeys?start=48.813896%2C2.392448&end=48.928378%2C2.162200&region_id=fr-paris"
+    base_url = "https://citymapper.com/api/7/journeys?"
+    end_url = "end=48.928378%2C2.162200&region_id=fr-paris"
+    valid_url = base_url + "start=48.813896%2C2.392448&" + end_url
     assert url == valid_url
+
+
+def test_get_params_simple():
+    src_exp = "48.813896%2C2.392448"
+    dst_exp = "48.928378%2C2.162200"
+    src, dest = climapper.get_params()
+    assert src_exp == src
+    assert dst_exp == dest
